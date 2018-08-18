e74f5e021e5f67b0a53637b4426012170b66654a
Aditya Sundar <37145366+sadidaa@users.noreply.github.com>
2018-08-19 05:08:25 +0530

    Update write.php

--- write.php
+++ write.php
@@ -12,7 +12,7 @@
 
     $con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
     //$sql = "insert into vivasa values((int)$kind,(float)$lat,(float)$longi,(float)$avrat,$review,(int)$noofrat,(int)$noofacc);";
-    $sql = "insert into vivasa values(2,3,4,5,"dwaadiiiiidwa",4,3);"
+    $sql = "insert into vivasa values(2,3,4,5,'dwaadiiiiidwa',4,3);"
 	$sql2 = "select count(*) from vivasa;";
 		//dawdwa
 	//$temp = mysqli_query($con);	
