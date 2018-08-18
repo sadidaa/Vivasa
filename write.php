11c5b4c9018c87a7e262dca1cd9d36277a41a09a
Aditya Sundar <37145366+sadidaa@users.noreply.github.com>
2018-08-19 03:16:30 +0530

    Add files via upload

---
+++ write.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<?php
+	$kind = $_GET['kind'];
+    $lat = $_GET['lat'];
+    $longi = $_GET['longi'];
+    $avrat = $_GET['rating'];
+    $review = $_GET['review'];
+    $noofrat = $_GET['noofrat'];
+    $noofacc = $_GET['noofacc'];
+
+    $con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
+    $sql = "insert into vivasa values($kind,$lat,$longi,$avrat,\"$review\",$noofrat,$noofacc);";
+    $sql2 = "select (*) from vivasa;";
+    mysqli_query($con,$sql);
+    //echo mysqli_query($con,$sql2);
+
+    echo $lat;
+
+    echo "Wrote to database \n\n";
+?>
+</body>
+</html>
