11bed8af61e80b23ada3b16400b6e2a25a1d52d9
Aditya Sundar <37145366+sadidaa@users.noreply.github.com>
2018-08-19 04:24:37 +0530

    Update write.php

--- write.php
+++ write.php
@@ -2,20 +2,20 @@
 <body>
 
 <?php
-	$kind = $_GET['kind'];
+	/*$kind = $_GET['kind'];
     $lat = $_GET['lat'];
     $longi = $_GET['longi'];
     $avrat = $_GET['rating'];
     $review = $_GET['review'];
     $noofrat = $_GET['noofrat'];
-    $noofacc = $_GET['noofacc'];
+    $noofacc = $_GET['noofacc'];*/
 
     $con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
     //$sql = "insert into vivasa values($kind,$lat,$longi,$avrat,$review,$noofrat,$noofacc);";
     $sql2 = "select count(*) from vivasa;";
 		
-	$temp = mysqli_query($con);	
-	$store = mysqli_query($sql2);
+	//$temp = mysqli_query($con);	
+	$store = mysqli_query($con,$sql2);
 	$row = mysqli_fetch_row($store);
 	echo $row[0];
     /*if($con->query($sql) === TRUE){
