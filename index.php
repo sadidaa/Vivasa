38e8ede576f7fe761524370d998f7906b72f6c62
Aditya <adityasundar99@gmail.com>
2018-08-19 01:26:41 +0530

    adawda

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <html>
 <body>
- <!-- <?php
-   $con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
-   $sql="CREATE TABLE table1(Username CHAR(30),Password CHAR(30),Role CHAR(30))";
-   $sql2="INSERT INTO table1 VALUES ('Dawawdwad','htd','25')";
-   $sql3 = "select * from table1";
-    //echo mysql_fetch_row(mysqli_query($sql3))[0] ;
-     
-$result = mysqli_query($con,"select * from table1");
-if (!$result) {
-    echo 'Could not run query: ' . mysql_error();
-    exit;
-}
-$row = mysqli_fetch_row($result);
 
-echo $row[0]; // 42
-echo $row[1]; // the email value
-
-echo $_GET['a'];
-?>
--->
 <?php
 	$kind = $_GET('kind');
     $lat = $_GET('lat');
