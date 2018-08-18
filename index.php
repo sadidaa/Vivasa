c872f9409a58a56c5343282d80dbdc60818e3102
Aditya <adityasundar99@gmail.com>
2018-08-19 01:51:18 +0530

    adawda

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 <body>
 
 <?php
-	$kind = $_GET('kind');
-    $lat = $_GET('lat');
-    $longi = $_GET('longi');
-    $avrat = $_GET('avrat');
-    $review = $_GET('review');
-    $noofrat = $_GET('noofrat');
-    $noofacc = $_GET('noofacc');
+	$kind = $_GET['kind'];
+    $lat = $_GET['lat'];
+    $longi = $_GET['longi'];
+    $avrat = $_GET['avrat'];
+    $review = $_GET['review'];
+    $noofrat = $_GET['noofrat'];
+    $noofacc = $_GET['noofacc'];
 
-    /*$con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
+    $con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
     $sql = mysqli_connect("insert into vivasa values($kind,$lat,$longi,$avrat,$review,$noofrat,$noofacc);");
 
-    mysqli_query($con,$sql);*/
+    mysqli_query($con,$sql);
 
     echo $lat;
 
