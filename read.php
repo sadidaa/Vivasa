a009862409a2996d598a60ea4522a531c6f6c1ad
Aditya Sundar <37145366+sadidaa@users.noreply.github.com>
2018-08-19 06:22:56 +0530

    Update read.php

--- read.php
+++ read.php
@@ -6,10 +6,12 @@ class entity{
   public $kind = -1;
   public $lat = 0;
   public $longi = 0;
-  public $avrate = 0;
-  public $noofacc = 0;
-  public $noofrate = 0;
+  public $avrat = 0;
   public $review = "";
+  public $noofrate = 0;
+  public $noofacc = 0;
+  
+  
 }
   /*function convert(arr)
   {
@@ -24,11 +26,11 @@ $obj = new entity();
   $sql = "select * from vivasa";
   $sql2 = "select count(*) from vivasa";
   
-  /*$temp = mysqli_query($con,$sql2);
+  $temp = mysqli_query($con,$sql2);
   $counta = mysqli_fetch_row($temp);
   $count = $counta[0];
   
-  	$out = mysqli_query($con,$sql);
+  	/*$out = mysqli_query($con,$sql);
   	$row = mysqli_fetch_row($out);*/
   
   
