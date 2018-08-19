c589ff0f3d1c0b961b6cfb5f1cbb64b4293830fd
Aditya Sundar <37145366+sadidaa@users.noreply.github.com>
2018-08-19 05:36:16 +0530

    Update write.php

--- write.php
+++ write.php
@@ -15,7 +15,7 @@
 	$a = "ooo";
 	
 	//The following works
-	$sql = "insert into vivasa values((int)$kind,3,4,5,$kind,4,3);";
+	$sql = "insert into vivasa values($kind,3,4,5,$kind,4,3);";
 	
 	$sql2 = "select count(*) from vivasa;";
 		//dawdwa
@@ -37,7 +37,7 @@
     //echo $lat;
 
    // echo "Wrote to database \n\n";*/
-	echo '2';
+	echo '1';
 	echo $review;
 	mysqli_close($con);
 	echo gettype($kind);
