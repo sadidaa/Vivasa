01229cbd520c01564fd04b525c3aaedb179f27f1
Aditya Sundar <37145366+sadidaa@users.noreply.github.com>
2018-08-19 06:20:17 +0530

    Update read.php

--- read.php
+++ read.php
@@ -3,21 +3,40 @@
 <?php
 
 class entity{
-  public $a = 5;
-  public $c = 6;
+  public $kind = -1;
+  public $lat = 0;
+  public $longi = 0;
+  public $avrate = 0;
+  public $noofacc = 0;
+  public $noofrate = 0;
+  public $review = "";
 }
+  function convert(arr)
+  {
+  }
+  
 $lat = $_GET['lat'];
 $longi = $_GET['longi'];
 
 $obj = new entity();
-$obj->a = 3;
-
-   echo json_encode($obj);
-
-
-
-
-echo "1";
+  
+ $con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
+  $sql = "select * from vivasa";
+  $sql2 = "select count(*) from vivasa";
+  
+  $temp = mysqli_query($con,$sql2);
+  $counta = mysqli_fetch_row($temp);
+  $count = $counta[0];
+  
+  	$out = mysqli_query($con,$sql);
+  	$row = mysqli_fetch_row($out);
+  
+  
+  
+  
+  echo json_encode($obj);
+echo "2";
+  echo $count;
 
 
 ?>
