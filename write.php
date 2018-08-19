c8435437d0e9c707f1e18933a4c773d633acd029
Aditya Sundar <37145366+sadidaa@users.noreply.github.com>
2018-08-19 05:35:39 +0530

    Update write.php

--- write.php
+++ write.php
@@ -11,11 +11,11 @@
     $noofacc = $_GET['noofacc'];
 
     $con=mysqli_connect("den1.mysql2.gear.host","vivasa","Se9kZQ-_GnnF","vivasa");
-    $sql = "insert into vivasa values((int)$kind,(float)$lat,(float)$longi,(float)$avrat,$review,(int)$noofrat,(int)$noofacc);";
+    //$sql = "insert into vivasa values((int)$kind,(float)$lat,(float)$longi,(float)$avrat,$review,(int)$noofrat,(int)$noofacc);";
 	$a = "ooo";
 	
 	//The following works
-	//$sql = "insert into vivasa values(2,3,4,5,$kind,4,3);";
+	$sql = "insert into vivasa values((int)$kind,3,4,5,$kind,4,3);";
 	
 	$sql2 = "select count(*) from vivasa;";
 		//dawdwa
@@ -37,7 +37,7 @@
     //echo $lat;
 
    // echo "Wrote to database \n\n";*/
-	echo '1';
+	echo '2';
 	echo $review;
 	mysqli_close($con);
 	echo gettype($kind);
